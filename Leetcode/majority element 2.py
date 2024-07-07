9ef4fb8a871460c48ddaccd06b52ff92e5bb2f11
shilajitgr <shshkush@gmail.com>
2024-07-07 08:07:50 +0530

    updated comment

--- Leetcode/majority element 2.py
+++ Leetcode/majority element 2.py
@@ -1,4 +1,5 @@
-# Extended Boyer Moore’s Voting Algorithm : Logic behind it, is that, if >n/3 is majority mark then only 2 majority elements are possible in total
+# Extended Boyer Moore’s Voting Algorithm : Logic behind it, is that,
+# if >n/3 is majority mark then only 2 majority elements are possible in total
 
 class Solution:
     def majorityElement(self, nums: list[int]) -> list[int]:
